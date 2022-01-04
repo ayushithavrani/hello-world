c4a392fbe39d211b2148f777f5a1637613b80f46
ayushithavrani <97086642+ayushithavrani@users.noreply.github.com>
2022-01-04 17:16:29 +0530

    initial comment of math.py

---
+++ math.py
@@ -0,0 +1,22 @@
+#add implementation
+def add(x,y):
+  return x+y
+
+#subtract implementation  
+def subtract(x,y):
+  return x-y
+   
+#multiply implementation
+def multiply(x,y):
+  return x*y
+   
+#divide implementation
+def divide(x,y):
+  if y==0:
+    return divide_by_0_error
+  else:
+    return x/y
+ 
+#square implementation 
+def square(x):
+  return x*x
